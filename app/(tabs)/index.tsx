94da1a763492d92899c1e88b58362a028265ffcb
Genesis Solution <000workemail000@gmail.com>
2025-01-04 16:33:45 -0500

    chore: remove loading component in home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,13 @@
-import {
-  Image,
-  ImageBackground,
-  SafeAreaView,
-  StyleSheet,
-  View,
-} from "react-native";
+import { ImageBackground, SafeAreaView, StyleSheet, View } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import LottieView from "lottie-react-native";
-import { useEffect, useState, Fragment } from "react";
-import { useGetKeywordsMutation } from "@/store/services/api";
-import scanLogger from "@/utils/scanLogger";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import BigButton from "@/components/BigButton";
-import { IKeyword } from "@/constants/types";
 import { router } from "expo-router";
 
 export default function HomeScreen() {
-  const [keywords, setKeywords] = useState<IKeyword[]>([]);
-  const [splashTimeout, setSplashTimeout] = useState(false);
-  const [getKeywords, { isLoading: isGettingKeywords }] =
-    useGetKeywordsMutation();
   const borderColor = useThemeColor({}, "text");
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSplashTimeout(true);
-    }, 2500);
-  }, []);
-
-  useEffect(() => {
-    const getAllKeywords = async () => {
-      try {
-        const allKeywords = await getKeywords({}).unwrap();
-        setKeywords(allKeywords);
-        scanLogger.log(`keywords: `, allKeywords);
-      } catch (error) {
-        scanLogger.error(
-          `Getting All Keywords Error: ${
-            (error as Error).message || "An unexpected error"
-          }`
-        );
-      }
-    };
-
-    getAllKeywords();
-  }, [getKeywords]);
 
   // const image = require("@/assets/images/scan-bg.png");
   const image = require("@/assets/images/yellow_bg.jpg");
@@ -89,56 +51,29 @@ export default function HomeScreen() {
       height: 300,
       alignSelf: "center",
     },
-    splashIcon: {
-      width: 200,
-      height: 200,
-      alignSelf: "center",
-    },
   });
 
-  const LoadingComponent = () => {
-    return (
-      <SafeAreaView
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "white",
-        }}
-      >
-        <Image
-          source={require("@/assets/images/icon.png")}
-          style={styles.splashIcon}
-        />
-      </SafeAreaView>
-    );
-  };
-
-  return !splashTimeout || isGettingKeywords ? (
-    <LoadingComponent />
-  ) : (
+  return (
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <SafeAreaView style={styles.container}>
-        <Fragment>
-          <View style={styles.titleContainer}>
-            <ThemedText type="title">Food Bug Scanner</ThemedText>
-          </View>
-          <View style={styles.barcodeContainer}>
-            <LottieView
-              source={require("@/assets/animations/barcode.json")}
-              autoPlay
-              style={styles.animation}
-            />
-          </View>
-          <View style={styles.scanBtnContainer}>
-            <BigButton
-              title="Tap to Scan"
-              onPress={() => {
-                router.replace("/scan");
-              }}
-            />
-          </View>
-        </Fragment>
+        <View style={styles.titleContainer}>
+          <ThemedText type="title">Food Bug Scanner</ThemedText>
+        </View>
+        <View style={styles.barcodeContainer}>
+          <LottieView
+            source={require("@/assets/animations/barcode.json")}
+            autoPlay
+            style={styles.animation}
+          />
+        </View>
+        <View style={styles.scanBtnContainer}>
+          <BigButton
+            title="Tap to Scan"
+            onPress={() => {
+              router.replace("/scan");
+            }}
+          />
+        </View>
       </SafeAreaView>
     </ImageBackground>
   );
