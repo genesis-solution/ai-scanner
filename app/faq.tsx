eb86ec6b58e5de6446bd0dc2a23b164bed6d8725
Genesis Solution <000workemail000@gmail.com>
2025-01-12 12:05:12 -0500

    chore: update FAQ

--- app/faq.tsx
+++ app/faq.tsx
@@ -5,23 +5,22 @@ import { Stack } from "expo-router";
 import { useTranslation } from "react-i18next";
 import { useThemeColor } from "@/hooks/useThemeColor";
 
-const faqData = [
-  {
-    id: 0,
-    title: "What is this app?",
-    content: "This app is a demo for managing settings in a React Native app.",
-  },
-  {
-    id: 1,
-    title: "How do I change the language?",
-    content:
-      "You can change the language in the settings under the General section.",
-  },
-  // Add more FAQs as needed
-];
-
 export default function FAQScreen() {
   const { t } = useTranslation();
+
+  const faqData = [
+    {
+      id: 0,
+      title: t("q0"),
+      content: t("a0"),
+    },
+    {
+      id: 1,
+      title: t("q1"),
+      content: t("a1"),
+    },
+    // Add more FAQs as needed
+  ];
 
   const styles = StyleSheet.create({
     container: {
